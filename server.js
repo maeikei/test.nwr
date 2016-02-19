beffb0a85ee4a94796452fe81e0d61a263de15f4
ma yingkui <maeikei@gmail.com>
2016-02-19 14:03:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,5 @@ var PeerServer = require('peer').PeerServer;
 var server = PeerServer({
   port: 9000,
   path: '/peer',
-  proxied: true,
-  ssl: {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt')
-  }
+  proxied: true
 });
