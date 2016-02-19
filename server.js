93abc3e48b4d21f66b30479b6edcd092286da7db
ma yingkui <maeikei@gmail.com>
2016-02-19 14:39:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var server = PeerServer({
   port: 9000
 });
 server.on('connection', function(id) {
-  console.log('connection' + id);
+  console.log('connection:id=<' + id + '>');
 });
 server.on('disconnect', function(id) {
-  console.log('disconnect' + id);
+  console.log('disconnect:id=<' + id + '>');
 });
